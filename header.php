df6df39468ed430c176000409897ce1c9882b25d
Simone Cirino <s-simone.cirino@istitutopilati.it>
2016-12-07 15:15:41 +0100

    Un po' di roba

---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+	ob_start();
+	session_start();
+	require_once 'dbconnect.php';
+	
+	// if session is not set this will redirect to login page
+	if( !isset($_SESSION['user']) ) {
+		header("Location: index.php");
+		exit;
+	}
+	// select loggedin users detail
+	$res=mysql_query("SELECT * FROM utenti WHERE username=".$_SESSION['user']);
+	$userRow=mysql_fetch_array($res);
+?>
+<!DOCTYPE html>
+	
+	<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="home.html">Gestione Simulimpresa</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="<?php if($page==1) echo "active"; ?>"><a href="magazzino.php">Magazzino</a></li>
+            <li class="<?php if($page==2) echo "active"; ?>"><a href="notYet.php?page=2">Finanza</a></li>
+            <li class="<?php if($page==3) echo "active"; ?>"><a href="notYet.php?page=3">Marketing</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+			  <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['username']; ?>&nbsp;<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Esci</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav> 
---
+++ notYet.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Benvenuto - <?php echo $userRow['username']; ?></title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+	<?php
+		$page=$_GET['page'];
+		include 'header.php'
+	?>
+
+	<div id="wrapper">
+
+	<div class="container">
+    
+    	<div class="page-header">
+    	<h3>Pagina non ancora realizzata</h3>
+    	</div>
+        
+        <div class="row">
+        <div class="col-lg-12">
+        <h1>O.o</h1>
+        </div>
+        </div>
+    
+    </div>
+    
+    </div>
+    
+    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
