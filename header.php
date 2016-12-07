dbcefa187204d33280cfc62bf821fa39da938dfe
Simone Cirino <s-simone.cirino@istitutopilati.it>
2016-12-07 15:17:47 +0100

    Un po' di roba

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="home.html">Gestione Simulimpresa</a>
+          <a class="navbar-brand" href="home.php">Gestione Simulimpresa</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
