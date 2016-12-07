aed4f7f482c8e2dcffba7ef5040cff3a9005ca37
Simone Cirino <s-simone.cirino@istitutopilati.it>
2016-12-07 16:20:18 +0100

    Un po' di roba

--- magazzino.php
+++ magazzino.php
@@ -1,45 +1,56 @@
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Benvenuto - <?php echo $userRow['username']; ?></title>
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
-<body>
+	<head>
+	
+		<style>
+			table, th, td {
+				border-collapse: collapse;
+			}
+			td:hover {
+				background-color:#cc6600;
+				color:blue;
+			}
+		</style>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Benvenuto - <?php echo $userRow['username']; ?></title>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+		<link rel="stylesheet" href="style.css" type="text/css" />
+	</head>
+	<body>
 
-	<?php
-		$page=1;
-		include 'header.php'
-	?>
+		<?php
+			$page=1;
+			include 'header.php'
+		?>
+		
+		<div id="wrapper">
 
-	<div id="wrapper">
+			<div class="container">
+		
+				<div class="page-header">
+				<h3>Magazzino</h3>
+				</div>
+			
+				<div class="panel panel-default">
+				<!-- Default panel contents -->
+					<div class="panel-heading">Elenco Prodotti</div>
 
-		<div class="container">
-    
-			<div class="page-header">
-			<h3>Magazzino</h3>
+					<!-- Table -->
+					<table class="table">
+						<tr>
+							<td>codici</td>
+							<td>descrizione prodotti</td>
+							<td>um</td>
+						</tr>
+					</table>
+				</div>
+		
 			</div>
-        
-			<div class="panel panel-default">
-			<!-- Default panel contents -->
-				<div class="panel-heading">Elenco Prodotti</div>
-
-				<!-- Table -->
-				<table class="table">
-					<tr>
-						<td>codici</td>
-						<td>descrizione prodotti</td>
-						<td>um</td>
-					</tr>
-				</table>
-			</div>
-    
+		
 		</div>
-    
-    </div>
-    
-    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    
-</body>
+		
+		<script src="assets/jquery-1.11.3-jquery.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		
+	</body>
 </html>
+
