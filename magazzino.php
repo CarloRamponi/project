d8289ffe931c540803307efb72eb200cacf0dee0
Carlo Ramponi <magocarlos1999@gmail.com>
2017-01-10 15:20:36 +0100

    N. 4 Script eliminaProdotti

--- magazzino.php
+++ magazzino.php
@@ -94,7 +94,7 @@
 				alert("BottoniModifica "+ n +"");
 				for (i=1;i<=n;i++){
 					if(document.getElementById("edit"+i+"")){
-						document.getElementById("edit"+i+"").outerHTML="<a href='aggiugniMagazzino.php?type='modifica'&id="+i+"'><span class='glyphicon glyphicon-edit' style='font-size:30px;'></span></a>";	
+						document.getElementById("edit"+i+"").outerHTML="<a href='aggiugniMagazzino.php?type='modifica'&id="+i+"' style='font-size:30px;'>\e065</a>";	
 					}
 				}
 			}
