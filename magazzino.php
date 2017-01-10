35c5f2692596edc1f6792eb46e4b3bb8471686dc
Simone Cirino <s-simone.cirino@istitutopilati.it>
2017-01-10 16:01:52 +0100

    Funzione elimina (Javascript) (4)

--- magazzino.php
+++ magazzino.php
@@ -7,7 +7,7 @@
 			}
 		</style>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>GS - Magazzino</title>
+		<!--<title>GS - Magazzino</title>-->
 		<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
 		<link rel="stylesheet" href="style.css" type="text/css" />
 	</head>
