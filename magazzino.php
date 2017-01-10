9c0e33b3502ebeae4584cf3fcfeb111d8fe9cee4
Carlo Ramponi <magocarlos1999@gmail.com>
2017-01-10 14:07:50 +0100

    N. 9 Aggiunti bottoni aggiungi, modifica ed elimina nella pagina del magazzino

--- magazzino.php
+++ magazzino.php
@@ -44,8 +44,7 @@
 							$res=mysql_query("SELECT * from prodotti");
 							$n=mysql_num_rows($res);
 							if($n!=0){
-								$i=0;
-								for(; $i< $n; $i++){
+								for($i = 0; $i< $n; $i++){
 									$prodotti[$i]=mysql_fetch_array($res);
 
 						?>
@@ -58,8 +57,9 @@
 						<?php
 									echo "<span id='del".$prodotti[$i]['id']."'></span>";
 									echo "<span id='edit".$prodotti[$i]['id']."'></span>";
+									$num=$prodotti[$i]['id'];
 								}
-								echo "<script>var numeroProdotti=".$prodotti[$i]['id'].";</script>";
+								echo "<script>var numeroProdotti=".$num.";</script>";
 							} else {
 						?>
 								<tr><td colspan="4">Nessun prodotto nel database!</td></tr>
