117ae37d9127fbddd04c98ed59b912a25dbe78ed
Carlo Ramponi <magocarlos1999@gmail.com>
2017-01-10 16:13:56 +0100

    Pagina aggiungi Magazzino

--- aggiungiMagazzino.php
+++ aggiungiMagazzino.php
@@ -45,28 +45,28 @@
             <div class="form-group">
             	<div class="input-group">
                 <span class="input-group-addon"><span class="glyphicon glyphicon-barcode"></span></span>
-            	<input type="integer" name="codice" class="form-control" placeholder="Codice prodotto" <?php if(type=="modifica") echo "value='".$prodotto['codice']."'";?>/>
+            	<input type="integer" name="codice" class="form-control" placeholder="Codice prodotto" <?php if($type=="modifica") echo "value='".$prodotto['codice']."'";?>/>
                 </div>
             </div>
 
             <div class="form-group">
             	<div class="input-group">
                 <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-            	<input type="text" name="descrizione" class="form-control" placeholder="Descrizione del prodotto" maxlength="150" <?php if(type=="modifica") echo "value='".$prodotto['descrizione']."'";?>/>
+            	<input type="text" name="descrizione" class="form-control" placeholder="Descrizione del prodotto" maxlength="150" <?php if($type=="modifica") echo "value='".$prodotto['descrizione']."'";?>/>
                 </div>
             </div>
 
              <div class="form-group">
             	<div class="input-group">
                 <span class="input-group-addon"><span class="glyphicon glyphicon-chevron-right"></span></span>
-            	<input type="text" name="iva" class="form-control" placeholder="Iva del prodotto" maxlength="150" <?php if(type=="modifica") echo "value='".$prodotto['iva']."'";?>/>
+            	<input type="text" name="iva" class="form-control" placeholder="Iva del prodotto" maxlength="150" <?php if($type=="modifica") echo "value='".$prodotto['iva']."'";?>/>
                 </div>
             </div>
 
              <div class="form-group">
             	<div class="input-group">
                 <span class="input-group-addon"><span class="glyphicon glyphicon-euro"></span></span>
-            	<input type="text" name="prezzo" class="form-control" placeholder="Prezzo unitario del prodotto" maxlength="150" <?php if(type=="modifica") echo "value='".$prodotto['prezzo']."'";?>/>
+            	<input type="text" name="prezzo" class="form-control" placeholder="Prezzo unitario del prodotto" maxlength="150" <?php if($type=="modifica") echo "value='".$prodotto['prezzo']."'";?>/>
                 </div>
             </div>
 
