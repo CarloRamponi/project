3069375876870002ebae4830e2ff35ddba09bc5f
Simone Cirino <s-simone.cirino@istitutopilati.it>
2017-01-10 15:10:46 +0100

    Funzione modifica (Javascript) (3)

--- magazzino.php
+++ magazzino.php
@@ -94,7 +94,7 @@
 				alert("BottoniModifica "+ n +"");
 				for (i=1;i<=n;i++){
 					if(document.getElementById("edit"+i+"")){
-						document.getElementById("edit"+i+"").outerHTML="<td><a class='glyphicon glyphicon-edit' href='aggiugniMagazzino.php?type='modifica'&id="+i+"'></a></td>";	
+						document.getElementById("edit"+i+"").outerHTML="<a class='glyphicon glyphicon-edit' href='aggiugniMagazzino.php?type='modifica'&id="+i+"'></a>";	
 					}
 				}
 			}
