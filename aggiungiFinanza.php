1bb6323c16afd26f20277d5ebdf7afbd75048143
Carlo Ramponi <magocarlos1999@gmail.com>
2017-02-01 17:50:21 +0100

    aggiunto un backup del database

---
+++ aggiungiFinanza.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Aggiungi Finanza</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+	<?php
+		$page=2;
+		include 'header.php'
+	?>
+
+	<div id="wrapper">
+
+	<div class="container">
+
+    	<div class="page-header">
+    	<h3>Aggiungi</h3>
+    	</div>
+
+         <form method="post" action="aggiungiFinanzaScript.php" autocomplete="off">
+
+    	<div class="col-md-12">
+
+        	<div class="form-group">
+            	<hr />
+            </div>
+
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+            	<input type="date" name="data" class="form-control" placeholder="data" maxlength="150" />
+                </div>
+            </div>
+
+						<div class="form-group">
+							<div class="input-group">
+								<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+							<input type="text" name="descrizione" class="form-control" placeholder="descrizione" maxlength="150" />
+								</div>
+						</div>
+
+						<div class="form-group">
+							<select class="form-control" name="tipo">
+							<option>Entrata</option>
+							<option>Uscita</option>
+						</select>
+						</div>
+
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-piggy-bank"></span></span>
+            	<input type="text" name="importo" class="form-control" placeholder="importo"/>
+                </div>
+            </div>
+
+            <div class="form-group">
+            	<button type="submit" class="btn btn-block btn-primary" name="btn-login">Aggiungi</button>
+            </div>
+
+
+        </div>
+
+    </form>
+
+    </div>
+
+    </div>
+
+    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ aggiungiFinanzaScript.php
@@ -0,0 +1,29 @@
+<?php
+
+    session_start();
+
+    if (!isset($_SESSION['user'])) {
+        header("Location: index.php");
+    }
+
+    require_once 'dbconnect.php';
+
+    $res=mysql_query("SELECT finanza FROM utenti WHERE username='".$_SESSION['user']."'");
+    $userRow=mysql_fetch_array($res);
+    $permessi=$userRow[0];
+
+    if($permessi==2){
+        $data = $_POST['data'];
+        $descrizione = $_POST['descrizione'];
+        $tipo = $_POST['tipo'];
+        $importo = $_POST['importo'];
+        if($dat == "" || $descrizione == "" || $tipo == "" || $importo = "")
+            header("location: aggiungiFinanza.php?error=vuoto");
+        else{
+            mysql_query("INSERT INTO finanza (data, descrizione, tipo, importo) VALUES ('".$data."', '".$descrizione."', '".$tipo."', ".$importo.");");
+        }
+    }
+
+    header("location: magazzino.php");
+
+?>
