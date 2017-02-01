c6a4db3c1394897e6cb73081de8a0db951971ae0
utente <s-matteo.fedrizzi@istitutopilati.it>
2017-02-01 14:33:17 +0100

    ho fatto la tabella per le materie prime

--- magazzino.php
+++ magazzino.php
@@ -107,6 +107,98 @@
 
 		<script src="assets/jquery-1.11.3-jquery.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
+		
+		
+		<!--oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo-->
+		
+		<script>var NumeroMateriePrime=0;</script>
+
+		<div id="wrapper">
+
+			<div class="container">
+
+				
+
+				<?php if($userRow['magazzino']){ ?>
+
+				<div class="panel panel-default">
+				<!-- Default panel contents -->
+					<div class="panel-heading">Log carico scarico materie prime</div>
+
+					<!-- Table -->
+					<table class="table">
+						<tr>
+							<th>Codice</th>
+							<th>Descrizione</th>
+							<th>Iva</th>
+							<th>Prezzo senza Iva</th>
+						</tr>
+						<?php
+							$res=mysql_query("SELECT * from prodotti");
+							$n=mysql_num_rows($res);
+							if($n!=0){
+								for($i = 0; $i< $n; $i++){
+									$prodotti[$i]=mysql_fetch_array($res);
+
+						?>
+									<tr>
+										<td><a href="caricoScaricoProdotti.php?codice=<?php echo $prodotti[$i]['codice']; ?>"><?php echo $prodotti[$i]['codice']; ?></a></td>
+										<td><?php echo $prodotti[$i]['descrizione']; ?></td>
+										<td><?php echo $prodotti[$i]['iva']; ?>%</td>
+										<td><?php echo $prodotti[$i]['prezzo']; ?>€</td>
+										<?php
+											echo "<td style='background-color:#FFFFFF' id='del".$prodotti[$i]['id']."'></td>";
+											echo "<td style='background-color:#FFFFFF' id='edit".$prodotti[$i]['id']."'></td>";
+										?>
+									</tr>
+						<?php
+									$num=$prodotti[$i]['id'];
+								}
+								echo "<script>var numeroProdotti=".$num.";</script>";
+							} else {
+						?>
+								<tr><td colspan="4">Nessun prodotto nel database!</td></tr>
+						<?php } ?>
+					</table>
+				</div>
+
+				<?php if($userRow['magazzino']==2){ ?>
+					<!-- Bottoni aggiungi e modifica! -->
+					<button class="btn btn-danger" onclick="bottoniElimina(numeroProdotti)">Elimina</button>
+					<button class="btn btn-warning" onclick="bottoniModifica(numeroProdotti)">Modifica</button>
+					<a class="btn btn-success" href = 'aggiungiMagazzino.php?type=aggiungi'>Aggiungi</a>
+				<?php } ?>
+			</div>
+
+			<?php } else { ?>
+				<!-- Non autorizzato a visualizzare! -->
+				Non autorizzato
+			<?php } ?>
+
+		</div>
+
+		<script>
+			function bottoniElimina(n){
+				for (i=1;i<=n;i++){
+					if(document.getElementById("del"+i+"")){
+						document.getElementById("del"+i+"").outerHTML="<td style='background-color:#FFFFFF;' id='eli"+i+"'><a href='eliminaProdotto.php?id="+i+"' style='font-size:25px;'><div class='glyphicon glyphicon-remove'></div></a></td>";
+					} else if(document.getElementById("eli"+i+""))
+							document.getElementById("eli"+i+"").outerHTML="<td style='background-color:#FFFFFF' id='del"+i+"'></td>";
+				}
+			}
+			function bottoniModifica(n){
+				for (i=1;i<=n;i++){
+					if(document.getElementById("edit"+i+"")){
+						document.getElementById("edit"+i+"").outerHTML="<td style='background-color:#FFFFFF;' id='mod"+i+"'><a href='aggiungiMagazzino.php?type=modifica&id="+i+"' style='font-size:25px;'><div class='glyphicon glyphicon-edit'></div></a></td>";
+					} else if(document.getElementById("mod"+i+""))
+							document.getElementById("mod"+i+"").outerHTML="<td style='background-color:#FFFFFF' id='edit"+i+"'></td>";
+				}
+			}
+		</script>
+
+		<script src="assets/jquery-1.11.3-jquery.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		
 
 	</body>
 </html>
