270e3dfb53a025d68b0b86c95d877128b1e9bc65
matteo fedrizzi <s-matteo.fedrizzi@istitutopilati.it>
2017-02-01 15:40:46 +0100

    Modificato carico scarico

--- caricoScaricoProdotti.php
+++ caricoScaricoProdotti.php
@@ -100,9 +100,8 @@
 
 
 				?>
-				<br><br>
 
-				<div class="panel panel-default" style="width:auto; float:right;">
+				<div class="panel panel-default" style="width:auto; float:right; display:inline;">
 					<div style="text-align:right; padding-right:20px; padding-left:20px;">
 						<h3>Totale: <?php echo $tot; ?></h3>
 					</div>
