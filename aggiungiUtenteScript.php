97eb37fc3c74b7fa66f536a164b51e9fa39faf2b
Carlo Ramponi <magocarlos1999@gmail.com>
2017-02-22 14:32:35 +0100

    Gestione Utenti

--- aggiungiUtenteScript.php
+++ aggiungiUtenteScript.php
@@ -8,7 +8,7 @@
 
   require_once 'dbconnect.php';
 
-	$res=mysql_query("SELECT admin FROM utenti WHERE username='".$_SESSION['user']."'");
+  $res=mysql_query("SELECT admin FROM utenti WHERE username='".$_SESSION['user']."'");
   $userRow=mysql_fetch_array($res);
   $permessi=$userRow[0];
 
@@ -45,7 +45,7 @@
 	$new_password2 = $_POST['new_password2'];
 
 	if($permessi==1){
-		$res=mysql_query("SELECT * FROM utenti WHERE username='".$nuovo_utente."'");
+		/*$res=mysql_query("SELECT * FROM utenti WHERE username='".$nuovo_utente."'");
 		$n=mysql_num_rows($res);
 		if($n==0){
 			if($nuovo_utente=="" || $new_password1=="" || $new_password2==""){
@@ -56,7 +56,7 @@
 				mysql_query("INSERT INTO utenti (username, password, admin, magazzino, finanza) values ('".$nuovo_utente."', '".$new_password1."', ".$admin.", ".$magazzino.", ".$finanza.");");
 		}
 		else
-			header("location: aggiungiUtente.php?error=utenteEsistente");
+			header("location: aggiungiUtente.php?error=utenteEsistente");*/
 	} else
 		header("location: index.php");
   }
