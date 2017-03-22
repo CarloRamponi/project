d26c63a4c87ac241757f5b9881facc7ad584074f
Carlo Ramponi <magocarlos1999@gmail.com>
2017-03-22 15:17:54 +0100

    Aggiunta nuova versione del database e creati gli script per aggiungere o modificare un cliente

---
+++ aggiungiClienteScript.php
@@ -0,0 +1,48 @@
+<?php
+
+  session_start();
+
+  if (!isset($_SESSION['user'])) {
+      header("Location: index.php");
+  }
+
+  require_once 'dbconnect.php';
+
+  $res=mysql_query("SELECT vendite FROM utenti WHERE username='".$_SESSION['user']."'");
+  $userRow=mysql_fetch_array($res);
+  $permessi=$userRow[0];
+
+  $codice=$_POST['codice'];
+  $nome=$_POST['nome'];
+  $pi=$_POST['pi'];
+  $via=$_POST['via'];
+  $cap=$_POST['cap'];
+  $citta=$_POST['citta'];
+  $telefono=$_POST['telefono'];
+  $fax=$_POST['fax'];
+  $mail=$_POST['mail'];
+  $iban=$_POST['iban'];
+  $banca=$_POST['banca'];
+  $pagamento=$_POST['pagamento'];
+  $scadenza=$_POST['scadenza'];
+  $annotazioni=$_POST['annotazioni'];
+  $orari=$_POST['orari'];
+  $sito=$_POST['sito'];
+
+
+	if($permessi==2){
+		$res=mysql_query("SELECT * FROM clienti WHERE codice='".$codice."'");
+		$n=mysql_num_rows($res);
+		if($n==0){
+			if($codice=="" || $nome==""){
+				header("Location: aggiungiUtente.php?error=vuoto&type=aggiungi");
+			} else {
+				mysql_query("INSERT INTO clienti (codice, nome, pi, via, cap, citta, telefono, fax, mail, iban, banca, pagamento, scadenza, annotazioni, orari, sito) values (".$codice.", '".$nome."', ".$pi.", '".$via."', ".$cap.", '".$citta."', ".$telefono.", ".$fax.", '".$mail."', ".$iban.", '".$banca."', '".$pagamento."', '".$scadenza."', '".$annotazioni."', '".$orari."', '".$sito."');");
+        header("Location: gestioneUtenti.php");
+      }
+		} else
+			header("Location: aggiungiUtente.php?error=clienteEsistente&type=aggiungi");
+	} else
+		header("Location: index.php");
+
+?>
---
+++ modificaClienteScript.php
@@ -0,0 +1,54 @@
+<?php
+
+  session_start();
+
+  if (!isset($_SESSION['user'])) {
+      header("Location: index.php");
+  }
+
+  if(!isset($_GET['id'])) {
+    header("Location: clienti.php");
+  } else {
+    $id=$_GET['id'];
+  }
+
+  require_once 'dbconnect.php';
+
+  $res=mysql_query("SELECT vendite FROM utenti WHERE username='".$_SESSION['user']."'");
+  $userRow=mysql_fetch_array($res);
+  $permessi=$userRow[0];
+
+  $codice=$_POST['codice'];
+  $nome=$_POST['nome'];
+  $pi=$_POST['pi'];
+  $via=$_POST['via'];
+  $cap=$_POST['cap'];
+  $citta=$_POST['citta'];
+  $telefono=$_POST['telefono'];
+  $fax=$_POST['fax'];
+  $mail=$_POST['mail'];
+  $iban=$_POST['iban'];
+  $banca=$_POST['banca'];
+  $pagamento=$_POST['pagamento'];
+  $scadenza=$_POST['scadenza'];
+  $annotazioni=$_POST['annotazioni'];
+  $orari=$_POST['orari'];
+  $sito=$_POST['sito'];
+
+	if($permessi==2){
+		$res=mysql_query("SELECT * FROM clienti WHERE codice='".$codice."'");
+		$n=mysql_num_rows($res);
+		if($n!=0){
+			if($codice=="" || $nome==""){
+				header("Location: aggiungiUtente.php?error=vuoto&type=modifica&id=".$id."");
+			} else {
+				mysql_query("UPDATE clienti SET codice=".$codice.", nome='".$nome."', pi=".$pi.", via='".$via."', cap=".$cap.", citta='".$citta."', telefono=".$telefono", fax=".$fax.", mail='".$mail."', iban=".$iban.", banca='".$banca."', pagamento='".$pagamento."', scadenza='".$scadenza."', annotazioni='".$annotazioni."', orari='".$orari."', sito='".$sito."';");
+        header("Location: clienti.php");
+      }
+		}
+	} else
+		header("Location: index.php");
+
+  ?>
+
+?>
